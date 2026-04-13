5959a10f8e1222863381ffb9e5aa9ed94366a483
Kampty <itskampty@gmail.com>
2026-04-13 17:27:44 +0300

    Update Open Graph and Twitter metadata

--- next-seo.config.js
+++ next-seo.config.js
@@ -2,23 +2,23 @@ export default {
     openGraph: {
         type: 'website',
         locale: 'en_IE',
-        title: 'next lnks by realvjy',
-        url: 'https://vjy.me',
-        description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk.',
+        title: 'Kampty',
+        url: 'https://floofix.net',
+        description: 'Hey, I’m Kampty — a furry content creator and occasional software dev',
         locale: 'en_EN',
         keywords: 'designer, product designer, illustrator, indian designer, vijay verma, realvjy, illlustrations, indian illustrator, ui designer india, sushi design system, zomato designer, 3dicons, uiprint, vijay realvjy, indian ui designer, design india, overlayz, nft art',
         images: [
             {
                 width: 1200,
                 height: 630,
-                url: `https://vjy.me/preview.jpg`,
+                url: `https://floofix.net/preview.jpg`,
             },
         ],
-        site_name: 'realvjy',
+        site_name: 'Kampty',
     },
     twitter: {
-        handle: '@realvjy',
-        site: 'vjy.me',
+        handle: '@floofixx',
+        site: 'floofix.net',
         cardType: 'summary_large_image',
     },
 };
